c027e55ba4655294a6a2c85f7e044bb2d5d11889
Lukasz Klimek <842586+lklimek@users.noreply.github.com>
2021-10-28 13:53:04 +0200

    feat: algorithm to select validators according to DIP-6

--- dash/sortable_validator.go
+++ dash/sortable_validator.go
@@ -9,15 +9,15 @@ import (
 	"github.com/tendermint/tendermint/types"
 )
 
-type SortableValidator struct {
+type sortableValidator struct {
 	*types.Validator
 	quorumHash tmbytes.HexBytes
 
-	sortKeyCached [32]byte
+	sortKeyCached []byte
 }
 
-func NewSortableValidator(validator *types.Validator, quorumHash tmbytes.HexBytes) SortableValidator {
-	sv := SortableValidator{
+func newSortableValidator(validator *types.Validator, quorumHash tmbytes.HexBytes) sortableValidator {
+	sv := sortableValidator{
 		Validator:  validator,
 		quorumHash: make([]byte, crypto.QuorumHashSize),
 	}
@@ -28,32 +28,58 @@ func NewSortableValidator(validator *types.Validator, quorumHash tmbytes.HexByte
 
 // Key calculates new key for this SortableValidator, which is SHA256(proTxHash, quorumHash), as per DIP-6.
 // Key is  cached.
-func (v SortableValidator) SortKey() []byte {
+func (v sortableValidator) SortKey() []byte {
 	if len(v.sortKeyCached) == 0 {
 		keyBytes := make([]byte, 0, len(v.ProTxHash)+len(v.quorumHash))
 		keyBytes = append(keyBytes, v.ProTxHash...)
 		keyBytes = append(keyBytes, v.quorumHash...)
-		v.sortKeyCached = sha256.Sum256(keyBytes)
+		keySHA := sha256.Sum256(keyBytes)
+		v.sortKeyCached = keySHA[:]
 	}
 
-	return v.sortKeyCached[:]
+	return v.sortKeyCached
 }
 
-// Equals returns info if this sortable validator is equal to the other one
-func (v SortableValidator) Equals(other SortableValidator) bool {
+// Equal returns info if this sortable validator is equal to the other one, based on ProTxHash
+func (v sortableValidator) Equal(other sortableValidator) bool {
 	return bytes.Equal(v.ProTxHash, other.ProTxHash)
 }
 
-type sortableValidatorList []SortableValidator
+type sortableValidatorList []sortableValidator
 
+func newSortableValidatorList(validators []*types.Validator, quorumHash tmbytes.HexBytes) sortableValidatorList {
+	ret := make(sortableValidatorList, 0, len(validators))
+	for _, validator := range validators {
+		sv := newSortableValidator(validator, quorumHash)
+		ret = append(ret, sv)
+	}
+	return ret
+}
+
+// Len implements sort.Interface. It returns length of sortableValidatorList
 func (vl sortableValidatorList) Len() int {
 	return len(vl)
 }
 
+// Less implements sort.Interface. It returns true when i'th element
+// of sortableValidatorList has lower key than j'th element.
 func (vl sortableValidatorList) Less(i, j int) bool {
 	return (bytes.Compare(vl[i].SortKey(), vl[j].SortKey()) < 0)
 }
 
+// Swap implements sort.Interface. It swaps i'th element with j'th element.
 func (vl sortableValidatorList) Swap(i, j int) {
 	vl[i], vl[j] = vl[j], vl[i]
+}
+
+// Index finds a validator on the list and returns its index.
+// Assumes the list is sorted. It uses sortableValidator.Equal() to compare validators.
+// Returns -1 when validator was not found.
+func (vl sortableValidatorList) Index(search sortableValidator) int {
+	for index, validator := range vl {
+		if search.Equal(validator) {
+			return index
+		}
+	}
+	return -1
 }
