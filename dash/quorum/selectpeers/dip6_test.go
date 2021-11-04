e621c46b15bc959f32fb208c8df943861dc6b5d8
Lukasz Klimek <842586+lklimek@users.noreply.github.com>
2021-11-04 09:11:15 +0100

    refactor: minor code formatting fixes

--- dash/quorum/selectpeers/dip6_test.go
+++ dash/quorum/selectpeers/dip6_test.go
@@ -23,67 +23,77 @@ func TestDIP6(t *testing.T) {
 			name:    "No validators",
 			me:      mock.NewValidator(0),
 			wantErr: true,
-		}, {
+		},
+		{
 			name:       "Only me",
 			validators: mock.NewValidators(1),
 			me:         mock.NewValidator(0),
 			quorumHash: mock.NewQuorumHash(0),
 			wantErr:    true,
-		}, {
+		},
+		{
 			name:       "4 validators",
 			validators: mock.NewValidators(4),
 			me:         mock.NewValidator(0),
 			quorumHash: mock.NewQuorumHash(0),
 			wantErr:    true,
-		}, {
+		},
+		{
 			name:            "5 validators",
 			validators:      mock.NewValidators(5),
 			me:              mock.NewValidator(0),
 			quorumHash:      mock.NewQuorumHash(0),
 			wantProTxHashes: mock.NewProTxHashes(0x01, 0x02),
 			wantLen:         2,
-		}, {
+		},
+		{
 			name:       "5 validators, not me",
 			validators: mock.NewValidators(5),
 			me:         mock.NewValidator(1000),
 			quorumHash: mock.NewQuorumHash(0),
 			wantErr:    true,
-		}, {
+		},
+		{
 			name:            "5 validators, different quorum hash",
 			validators:      mock.NewValidators(5),
 			me:              mock.NewValidator(0),
 			quorumHash:      mock.NewQuorumHash(1),
 			wantProTxHashes: mock.NewProTxHashes(0x01, 0x03),
 			wantLen:         2,
-		}, {
+		},
+		{
 			name:            "8 validators",
 			validators:      mock.NewValidators(6),
 			me:              mock.NewValidator(0),
 			quorumHash:      mock.NewQuorumHash(0),
 			wantProTxHashes: mock.NewProTxHashes(0x01, 0x02),
 			wantLen:         2,
-		}, {
+		},
+		{
 			name:            "9 validators",
 			validators:      mock.NewValidators(9),
 			me:              mock.NewValidator(0),
 			quorumHash:      mock.NewQuorumHash(0),
 			wantProTxHashes: mock.NewProTxHashes(0x06, 0x01, 0x02),
 			wantLen:         3,
-		}, {
+		},
+		{
 			name:            "37 validators",
 			validators:      mock.NewValidators(37),
 			me:              mock.NewValidator(0),
 			quorumHash:      mock.NewQuorumHash(0),
 			wantProTxHashes: mock.NewProTxHashes(0xc, 0xa, 0x15, 0x19, 0x1d),
 			wantLen:         5,
-		}, {
+		},
+		{
 			name:            "37 validators, I am last",
 			validators:      mock.NewValidators(37),
 			me:              mock.NewValidator(36),
 			quorumHash:      mock.NewQuorumHash(0),
 			wantProTxHashes: mock.NewProTxHashes(0x08, 0x1b, 0x22, 0x23, 0x16),
 			wantLen:         5,
-		}, {
+		},
+		{
 			name:       "37 validators, not me",
 			validators: mock.NewValidators(37),
 			me:         mock.NewValidator(37),
--- dash/quorum/selectpeers/sorted_validator_list_test.go
+++ dash/quorum/selectpeers/sorted_validator_list_test.go
@@ -14,22 +14,26 @@ func Test_sortedValidatorList_Index(t *testing.T) {
 		list   sortedValidatorList
 		search sortableValidator
 		want   int
-	}{{
-		name:   "miss",
-		list:   newSortedValidatorList(mock.NewValidators(5), mock.NewQuorumHash(0)),
-		search: newSortableValidator(*mock.NewValidator(10), mock.NewQuorumHash(0)),
-		want:   -1,
-	}, {
-		name:   "i=0",
-		list:   newSortedValidatorList(mock.NewValidators(500), mock.NewQuorumHash(0)),
-		search: newSortableValidator(*mock.NewValidator(0), mock.NewQuorumHash(0)),
-		want:   0,
-	}, {
-		name:   "i=4",
-		list:   newSortedValidatorList(mock.NewValidators(500), mock.NewQuorumHash(2054231)),
-		search: newSortableValidator(*mock.NewValidator(4), mock.NewQuorumHash(2054231)),
-		want:   4,
-	}}
+	}{
+		{
+			name:   "miss",
+			list:   newSortedValidatorList(mock.NewValidators(5), mock.NewQuorumHash(0)),
+			search: newSortableValidator(*mock.NewValidator(10), mock.NewQuorumHash(0)),
+			want:   -1,
+		},
+		{
+			name:   "i=0",
+			list:   newSortedValidatorList(mock.NewValidators(500), mock.NewQuorumHash(0)),
+			search: newSortableValidator(*mock.NewValidator(0), mock.NewQuorumHash(0)),
+			want:   0,
+		},
+		{
+			name:   "i=4",
+			list:   newSortedValidatorList(mock.NewValidators(500), mock.NewQuorumHash(2054231)),
+			search: newSortableValidator(*mock.NewValidator(4), mock.NewQuorumHash(2054231)),
+			want:   4,
+		},
+	}
 	// nolint:scopelint
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
