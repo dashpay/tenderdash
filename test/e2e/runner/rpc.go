fe1ac996f7c2c18ce47d50be2c11d97ae33feb81
shotonoff <dmitrii.golubev@dash.org>
2022-01-20 15:04:16 +0100

    refactor: reduce timeout for requesting node status from e2e runner

--- test/e2e/runner/rpc.go
+++ test/e2e/runner/rpc.go
@@ -74,7 +74,7 @@ func waitForHeight(ctx context.Context, testnet *e2e.Testnet, height int64) (*ty
 					clients[node.Name] = client
 				}
 
-				wctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+				wctx, cancel := context.WithTimeout(ctx, 2*time.Second)
 				defer cancel()
 				result, err := client.Status(wctx)
 				if err != nil {
