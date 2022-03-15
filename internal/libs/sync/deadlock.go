c467c5125e9cb3932495a5041ca88a6dc2fba8c8
Lukasz Klimek <842586+lklimek@users.noreply.github.com>
2022-03-15 15:13:33 +0100

    fix(sync): revert deadlock logic changes

--- internal/libs/sync/deadlock.go
+++ internal/libs/sync/deadlock.go
@@ -10,48 +10,9 @@ import (
 // A Mutex is a mutual exclusion lock.
 type Mutex struct {
 	deadlock.Mutex
-	muxHelper
-}
-
-func (m *Mutex) Lock() {
-	m.muxHelper.beforeLock()
-	m.Mutex.Lock()
-	m.muxHelper.afterLock()
-}
-func (m *Mutex) Unlock() {
-	m.muxHelper.beforeUnlock()
-	m.Mutex.Unlock()
-	m.muxHelper.afterUnlock()
 }
 
 // An RWMutex is a reader/writer mutual exclusion lock.
 type RWMutex struct {
 	deadlock.RWMutex
-	readMuxHelper  muxHelper
-	writeMuxHelper muxHelper
-}
-
-func (m *RWMutex) Lock() {
-	m.writeMuxHelper.beforeLock()
-	m.RWMutex.Lock()
-	m.writeMuxHelper.afterLock()
-
-}
-func (m *RWMutex) Unlock() {
-	// no need to lock timerLock, as we are already locked by RWMutex.Lock
-	m.writeMuxHelper.beforeUnlock()
-	m.RWMutex.Unlock()
-	m.writeMuxHelper.afterUnlock()
-}
-
-func (m *RWMutex) RLock() {
-	m.readMuxHelper.beforeLock()
-	m.RWMutex.RLock()
-	m.readMuxHelper.afterLock()
-}
-
-func (m *RWMutex) RUnlock() {
-	m.readMuxHelper.beforeUnlock()
-	m.RWMutex.RUnlock()
-	m.readMuxHelper.afterUnlock()
 }
