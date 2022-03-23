c92352be3d6852f20b1d342cb526079f45202c98
shotonoff <dmitrii.golubev@dash.org>
2022-03-23 12:33:07 +0100

    chore: remove a few files that were committed by accident

--- test/e2e/log.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-#cd networks/island && docker-compose logs -f
-#cd networks/ci && docker-compose logs -f
-cd networks/dashcore && docker-compose logs -f
-#cd networks/rotate && docker-compose logs -f
-#cd networks/ci2 && docker-compose logs -f
