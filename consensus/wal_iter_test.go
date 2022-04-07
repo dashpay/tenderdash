7a36dcf117c1034e77b167c110fe9c044e4ba1ca
shotonoff <dmitrii.golubev@dash.org>
2022-04-07 12:16:43 +0200

    feat: improve was_iter_test.go

--- consensus/wal_iter_test.go
+++ consensus/wal_iter_test.go
@@ -1,23 +1,28 @@
 package consensus
 
 import (
+	"errors"
 	"fmt"
 	"io"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 	tmtypes "github.com/tendermint/tendermint/types"
 )
 
-func TestWalIter(t *testing.T) {
+func TestWalIter_Next(t *testing.T) {
 	testCases := []struct {
-		input   []Message
+		input   []interface{}
 		want    []Message
 		wantLen int
 	}{
 		{
+			wantLen: 0,
+		},
+		{
 			wantLen: 3,
-			input: []Message{
+			input: []interface{}{
 				&ProposalMessage{Proposal: &tmtypes.Proposal{Height: 1000, Round: 0}},
 				&ProposalMessage{Proposal: &tmtypes.Proposal{Height: 1001, Round: 0}},
 				&ProposalMessage{Proposal: &tmtypes.Proposal{Height: 1002, Round: 0}},
@@ -26,6 +31,23 @@ func TestWalIter(t *testing.T) {
 				&ProposalMessage{Proposal: &tmtypes.Proposal{Height: 1000, Round: 0}},
 				&ProposalMessage{Proposal: &tmtypes.Proposal{Height: 1001, Round: 0}},
 				&ProposalMessage{Proposal: &tmtypes.Proposal{Height: 1002, Round: 0}},
+			},
+		},
+		{
+			wantLen: 5,
+			input: []interface{}{
+				&VoteMessage{Vote: &tmtypes.Vote{Height: 0, Round: 0}},
+				&ProposalMessage{Proposal: &tmtypes.Proposal{Height: 1000, Round: 0}},
+				&BlockPartMessage{Height: 1000, Round: 0},
+				&VoteMessage{Vote: &tmtypes.Vote{Height: 1000, Round: 0}},
+				&VoteMessage{Vote: &tmtypes.Vote{Height: 1000, Round: 0}},
+			},
+			want: []Message{
+				&VoteMessage{Vote: &tmtypes.Vote{Height: 0, Round: 0}},
+				&ProposalMessage{Proposal: &tmtypes.Proposal{Height: 1000, Round: 0}},
+				&BlockPartMessage{Height: 1000, Round: 0},
+				&VoteMessage{Vote: &tmtypes.Vote{Height: 1000, Round: 0}},
+				&VoteMessage{Vote: &tmtypes.Vote{Height: 1000, Round: 0}},
 			},
 		},
 		{
@@ -41,7 +63,7 @@ func TestWalIter(t *testing.T) {
 				&ProposalMessage{Proposal: &tmtypes.Proposal{Height: 1002, Round: 0}},
 				&VoteMessage{Vote: &tmtypes.Vote{Height: 1002, Round: 0}},
 			},
-			input: []Message{
+			input: []interface{}{
 				&VoteMessage{Vote: &tmtypes.Vote{Height: 0, Round: 0}},
 				&ProposalMessage{Proposal: &tmtypes.Proposal{Height: 1000, Round: 0}},
 				&VoteMessage{Vote: &tmtypes.Vote{Height: 1000, Round: 0}},
@@ -62,10 +84,27 @@ func TestWalIter(t *testing.T) {
 				&VoteMessage{Vote: &tmtypes.Vote{Height: 1002, Round: 0}},
 			},
 		},
+		{
+			wantLen: 9,
+			input:   generateMessageSequence(1000, 1002, 1000),
+			want: []Message{
+				&ProposalMessage{Proposal: &tmtypes.Proposal{Height: 1000, Round: 1000}},
+				&BlockPartMessage{Height: 1000, Round: 1000},
+				&VoteMessage{Vote: &tmtypes.Vote{Height: 1000, Round: 1000}},
+
+				&ProposalMessage{Proposal: &tmtypes.Proposal{Height: 1001, Round: 1000}},
+				&BlockPartMessage{Height: 1001, Round: 1000},
+				&VoteMessage{Vote: &tmtypes.Vote{Height: 1001, Round: 1000}},
+
+				&ProposalMessage{Proposal: &tmtypes.Proposal{Height: 1002, Round: 1000}},
+				&BlockPartMessage{Height: 1002, Round: 1000},
+				&VoteMessage{Vote: &tmtypes.Vote{Height: 1002, Round: 1000}},
+			},
+		},
 	}
 	for i, tc := range testCases {
 		t.Run(fmt.Sprintf("test-case #%d", i), func(t *testing.T) {
-			it := newWalIter(&walMessages{msgs: tc.input})
+			it := newWalIter(&mockWALReader{ret: tc.input})
 			cnt := 0
 			for it.Next() {
 				msg := it.Value()
@@ -79,20 +118,78 @@ func TestWalIter(t *testing.T) {
 	}
 }
 
-type walMessages struct {
-	msgs []Message
-	pos  int
+func TestWalIter_Err(t *testing.T) {
+	testCases := []struct {
+		err  error
+		want string
+	}{
+		{
+			err: io.EOF,
+		},
+		{
+			err:  errors.New("reader error"),
+			want: "reader error",
+		},
+	}
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("test-case #%d", i), func(t *testing.T) {
+			it := newWalIter(&mockWALReader{ret: []interface{}{
+				tc.err,
+			}})
+			require.False(t, it.Next())
+			requireError(t, tc.want, it.Err())
+		})
+	}
 }
 
-func (w *walMessages) Decode() (*TimedWALMessage, error) {
-	l := len(w.msgs)
+func requireError(t *testing.T, want string, err error) {
+	if err != nil {
+		require.True(t, strings.Contains(err.Error(), want))
+		return
+	}
+	require.Equal(t, "", want, "error must not be nil")
+}
+
+type mockWALReader struct {
+	ret []interface{}
+	pos int
+}
+
+func (w *mockWALReader) Decode() (*TimedWALMessage, error) {
+	l := len(w.ret)
 	if l == 0 || w.pos >= l {
 		return nil, io.EOF
 	}
-	msg := w.msgs[w.pos]
-	w.pos++
-	twm := TimedWALMessage{
-		Msg: msgInfo{Msg: msg},
+	r := w.ret[w.pos]
+	var (
+		twm *TimedWALMessage
+		err error
+	)
+	switch val := r.(type) {
+	case Message:
+		twm = &TimedWALMessage{
+			Msg: msgInfo{Msg: val},
+		}
+	case error:
+		err = val
+	default:
+		err = fmt.Errorf("unsupported a type, allowed [Message, error]; given %T", r)
 	}
-	return &twm, nil
+	w.pos++
+	return twm, err
+}
+
+func generateMessageSequence(heightStart, heightEnd int64, roundEnd int32) []interface{} {
+	var msgs []interface{}
+	for h := heightStart; h <= heightEnd; h++ {
+		for r := int32(0); r <= roundEnd; r++ {
+			msgs = append(
+				msgs,
+				&ProposalMessage{Proposal: &tmtypes.Proposal{Height: h, Round: r}},
+				&BlockPartMessage{Height: h, Round: r},
+				&VoteMessage{Vote: &tmtypes.Vote{Height: h, Round: r}},
+			)
+		}
+	}
+	return msgs
 }
