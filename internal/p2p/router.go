55224e8af7d0ca73df9dd754d121b5888f2257b1
shotonoff <dmitrii.golubev@dash.org>
2022-10-13 13:25:01 +0200

    fix: remove Router.runWithPeerMutex

--- internal/p2p/router.go
+++ internal/p2p/router.go
@@ -515,8 +515,8 @@ func (r *Router) openConnection(ctx context.Context, conn Connection) {
 		return
 	}
 
-	proTxHashSetter := SetProTxHashToPeerInfo(peerInfo.ProTxHash)
-	if err := r.runWithPeerMutex(func() error { return r.peerManager.Accepted(peerInfo.NodeID, proTxHashSetter) }); err != nil {
+	err = r.peerManager.Accepted(peerInfo.NodeID, SetProTxHashToPeerInfo(peerInfo.ProTxHash))
+	if err != nil {
 		r.logger.Error("failed to accept connection",
 			"op", "incoming/accepted", "peer", peerInfo.NodeID, "err", err)
 		return
@@ -601,8 +601,8 @@ func (r *Router) connectPeer(ctx context.Context, address NodeAddress) {
 		return
 	}
 
-	proTxHashSetter := SetProTxHashToPeerInfo(peerInfo.ProTxHash)
-	if err := r.runWithPeerMutex(func() error { return r.peerManager.Dialed(address, proTxHashSetter) }); err != nil {
+	err = r.peerManager.Dialed(address, SetProTxHashToPeerInfo(peerInfo.ProTxHash))
+	if err != nil {
 		r.logger.Error("failed to dial peer", "op", "outgoing/dialing", "peer", address.NodeID, "err", err)
 		r.peerManager.dialWaker.Wake()
 		conn.Close()
