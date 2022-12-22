f8c8b9172b123a37554261e07083ca64786d06a3
Lukasz Klimek <842586+lklimek@users.noreply.github.com>
2022-12-22 14:43:07 +0100

    feat(cmd): add deadlock detect info to `tenderdash version -v`

--- cmd/tenderdash/commands/version.go
+++ cmd/tenderdash/commands/version.go
@@ -2,17 +2,33 @@ package commands
 
 import (
 	"fmt"
+	"runtime"
 
+	"github.com/sasha-s/go-deadlock"
 	"github.com/spf13/cobra"
 
 	"github.com/tendermint/tendermint/version"
 )
 
 // VersionCmd ...
-var VersionCmd = &cobra.Command{
-	Use:   "version",
-	Short: "Show version info",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(version.TMCoreSemVer)
-	},
-}
+var VersionCmd *cobra.Command = func() *cobra.Command {
+	verbose := false
+	cmd := &cobra.Command{
+		Use:   "version",
+		Short: "Show version info",
+
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println(version.TMCoreSemVer)
+			if verbose {
+				fmt.Println("Go version: " + runtime.Version())
+				if deadlock.Opts.Disable {
+					fmt.Println("Deadlock detection: disabled")
+				} else {
+					fmt.Println("Deadlock detection: enabled, timeout: ", deadlock.Opts.DeadlockTimeout.String())
+				}
+			}
+		},
+	}
+	cmd.Flags().BoolVarP(&verbose, "verbose", "v", false, "display additional compilation info")
+	return cmd
+}()
