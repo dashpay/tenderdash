604fbf3009c6a1fdbb1fd5677c8fb3cd100c528d
lklimek <842586+lklimek@users.noreply.github.com>
2023-01-03 13:07:35 +0100

    build: enable deadlock detection on -dev docker images  (#540)
    * build: enable deadlock detection in dev builds
    
    * feat(cmd): add deadlock detect info to `tenderdash version -v`
    
    * build(docker): expose arg TENDERMINT_BUILD_OPTIONS
    
    * build: differentiate docker image type (final/dev)
    
    * chore: docker.yml
    
    * build(docker): adapt for layer caching
    
    * chore(github): TENDERMINT_BUILD_OPTIONS
    
    * chore: gh workflow
    
    * chore: docker build
    
    * build(docker): improve docker layered caching
    
    * chore: working on bls-signatures build.sh
    
    * chore: build bls
    
    * chore: docker caching
    
    * chore(cmd): s/fmt.Println/cmd.Println/ in version.go

--- third_party/bls-signatures/build.sh
+++ third_party/bls-signatures/build.sh
@@ -4,24 +4,29 @@ SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 SRC_PATH="$SCRIPT_PATH/src"
 BUILD_PATH="$SCRIPT_PATH/build"
 BLS_SM_PATH="third_party/bls-signatures/src"
-BLS_GIT_REPO="https://github.com/dashpay/bls-signatures.git"
+# FIXME: Use shotonoff's repo, as develop_0.1 was removed from dashpay/bls-signatures
+# We can get back to dashpay/bls-signatures in tenderdash v0.11, as we will use new version of bls-signatures there
+# BLS_GIT_REPO="https://github.com/dashpay/bls-signatures.git"
+BLS_GIT_REPO="https://github.com/shotonoff/bls-signatures.git"
 BLS_GIT_BRANCH="develop_0.1"
 
-git submodule update --init $BLS_SM_PATH
-if [ $? -ne 0 ]; then
+set -ex
+
+if ! git submodule update --init "${BLS_SM_PATH}" ; then
 	echo "It looks like this source code is not tracked by git."
-	echo "As a fallback scenario we will fetch \"$BLS_GIT_BRANCH\" branch \"$BLS_GIT_REPO\" library."
+	echo "As a fallback scenario we will fetch \"${BLS_GIT_BRANCH}\" branch \"${BLS_GIT_REPO}\" library."
 	echo "We would recommend to clone of this project rather than using a release archive."
-	git clone --single-branch --branch $BLS_GIT_BRANCH $BLS_GIT_REPO $BLS_SM_PATH
+	rm -r  "${BLS_SM_PATH}" || true
+	git clone --single-branch --branch "${BLS_GIT_BRANCH}" "${BLS_GIT_REPO}" "${BLS_SM_PATH}" 
 fi
 
 # Create folders for source and build data
-mkdir -p $BUILD_PATH
+mkdir -p "${BUILD_PATH}"
 
 # Configurate the library build
-cmake -B $BUILD_PATH -S $SRC_PATH
+cmake -B "${BUILD_PATH}" -S "${SRC_PATH}"
 
 # Build the library
-make -C $BUILD_PATH chiabls
+make -C "${BUILD_PATH}" chiabls
 
 exit 0
