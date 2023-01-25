5e9822f2fefe3a8cee89e01cb0c57b69deccb4b0
Lukasz Klimek <842586+lklimek@users.noreply.github.com>
2023-01-25 15:34:35 +0100

    test(ed25519): sync SLIP10/BIP39 test format with js lib

--- crypto/ed25519/slip10_test.go
+++ crypto/ed25519/slip10_test.go
@@ -1,6 +1,7 @@
 package ed25519
 
 import (
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,41 +13,42 @@ func TestNewFromMnemonic(t *testing.T) {
 		mnemonic        string
 		passphrase      string
 		deriviationPath string
-		pubkeyAddress   string
+		pubkeyHex       string
 	}
 
 	testCases := []testCase{
 		{
 			mnemonic:        "injury urban mention wide beach cereal industry rule capital picnic aerobic hotel cable okay curious",
-			passphrase:      "zażółć",
+			passphrase:      "",
 			deriviationPath: "m/9'/5'/3'/4'/0'",
-			pubkeyAddress:   "D34C6CF6BD79C7822C1E40950C279CE3A03126F1",
+			pubkeyHex:       "98549849af5468eabf377e0c7eb493c00daa37b7ca6fa18989fb430ca83ff58d",
 		},
 		{
 			mnemonic:        "injury urban mention wide beach cereal industry rule capital picnic aerobic hotel cable okay curious",
-			passphrase:      "",
+			passphrase:      "zazolc",
 			deriviationPath: "m/9'/5'/3'/4'/0'",
-			pubkeyAddress:   "7F9DC230E64396E96BE0061A0B6F36AF2F89ACA0",
+			pubkeyHex:       "9041e8deaddde890e6041b28a9239aa73bdaf7743531646ede9f198e53b1bed6",
 		},
 		{
 			mnemonic:        "pave review wealth sign level crane stick sick remind club ordinary gauge despair jeans lava anxiety accuse ship buddy price globe",
 			passphrase:      "",
 			deriviationPath: "m/9'/5'/3'/4'/0'",
-			pubkeyAddress:   "449E1D79E782DEB8AE7B77E72FBFB52561BD0AC5",
+			pubkeyHex:       "fc0399d47050e3eaf99e0023f987551438137d8d550b2e17f6bc8e48dece917d",
 		},
 		{
 			mnemonic:        "pave review wealth sign level crane stick sick remind club ordinary gauge despair jeans lava anxiety accuse ship buddy price globe",
 			passphrase:      "",
 			deriviationPath: "m/9'/5'/3'/4'/1'",
-			pubkeyAddress:   "29942C234C0871509840376056E27C778FEFDC71",
+			pubkeyHex:       "eb0c4ccd7b422fa880bb4cd37eb04bf7086dfa1991588f2d63840e32cac9c1b8",
 		},
 	}
-	for _, tc := range testCases {
-		t.Run("", func(t *testing.T) {
+	for i, tc := range testCases {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
 			pk, err := FromBip39Mnemonic(tc.mnemonic, tc.passphrase, tc.deriviationPath)
 			assert.NoError(t, err)
 			require.NotNil(t, pk)
-			assert.Equal(t, tc.pubkeyAddress, pk.PubKey().Address().String())
+			assert.Equal(t, tc.pubkeyHex, pk.PubKey().HexString())
+			// t.Logf("%d pubkey hex: %s\n", i, pk.PubKey().HexString())
 		})
 	}
 }
