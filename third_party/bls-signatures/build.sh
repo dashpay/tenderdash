90e118217deb113450b905a4b09de5de1a36e0be
shotonoff <dmitrii.golubev@dash.org>
2023-02-15 13:48:56 +0100

    build: update bls tag in build script, remove bls install operation from Dockerfile

--- third_party/bls-signatures/build.sh
+++ third_party/bls-signatures/build.sh
@@ -5,7 +5,7 @@ SRC_PATH="${SCRIPT_PATH}/src"
 BUILD_PATH="${SCRIPT_PATH}/build"
 BLS_SM_PATH="third_party/bls-signatures/src"
 BLS_GIT_REPO="https://github.com/dashpay/bls-signatures.git"
-BLS_GIT_BRANCH=${BLS_GIT_BRANCH:-"1.2.5"}
+BLS_GIT_BRANCH=${BLS_GIT_BRANCH:-"1.2.6"}
 
 set -e
 
@@ -14,7 +14,7 @@ if ! git submodule update --init "${BLS_SM_PATH}" ; then
 	echo "As a fallback scenario we will fetch \"${BLS_GIT_BRANCH}\" branch \"${BLS_GIT_REPO}\" library."
 	echo "We would recommend to clone of this project rather than using a release archive."
 	rm -r  "${BLS_SM_PATH}" || true
-	git clone --single-branch --branch "${BLS_GIT_BRANCH}" "${BLS_GIT_REPO}" "${BLS_SM_PATH}" 
+	git clone --single-branch --branch "${BLS_GIT_BRANCH}" "${BLS_GIT_REPO}" "${BLS_SM_PATH}"
 fi
 
 # Create folders for source and build data
