9d450743e6adbf2484a2edb7fbad5c7ca6f94117
shotonoff <dmitrii.golubev@dash.org>
2023-03-06 17:38:26 +0100

    chore: add examples in promise/example_test.go and docs comments

--- libs/promise/promise_test.go
+++ libs/promise/promise_test.go
@@ -3,9 +3,13 @@ package promise
 import (
 	"errors"
 	"fmt"
+	"strconv"
 	"testing"
 
+	sync "github.com/sasha-s/go-deadlock"
 	"github.com/stretchr/testify/require"
+
+	tmrequire "github.com/tendermint/tendermint/internal/test/require"
 )
 
 func TestPromise(t *testing.T) {
@@ -31,15 +35,33 @@ func TestPromise(t *testing.T) {
 		},
 	}
 	for i, tc := range testCases {
-		t.Run(fmt.Sprintf("test-case %d", i), func(t *testing.T) {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
 			p := New(tc.exec)
 			res, err := p.Await()
-			if tc.wantErr != "" {
-				require.Errorf(t, err, tc.wantErr)
-				return
-			}
-			require.NoError(t, err)
+			tmrequire.Error(t, tc.wantErr, err)
 			require.Equal(t, tc.wantVal, res.value)
 		})
 	}
+}
+
+func TestThen(t *testing.T) {
+	type (
+		T message[int]
+		M message[string]
+	)
+	promise := New[T](func(resolve func(data T), reject func(err error)) {
+		resolve(T{val: 100})
+	})
+	wrappedPromise := Then[T, M](promise, func(msg T) M {
+		return M{val: strconv.Itoa(msg.val)}
+	})
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		res, err := wrappedPromise.Await()
+		require.NoError(t, err)
+		require.Equal(t, "100", res.val)
+	}()
+	wg.Wait()
 }
