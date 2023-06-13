200c050690a4927cec7991e459ddd5592257a028
lklimek <842586+lklimek@users.noreply.github.com>
2023-06-13 12:40:43 +0200

    fix(proto)!: remove unused SIGNED_MSG_TYPE_COMMIT (#638)

--- version/version.go
+++ version/version.go
@@ -11,7 +11,7 @@ const (
 	// when not using git describe. It is formatted with semantic versioning.
 	TMVersionDefault = "0.12.0-dev.2"
 	// ABCISemVer is the semantic version of the ABCI library
-	ABCISemVer = "0.20.0"
+	ABCISemVer = "0.21.0"
 
 	ABCIVersion = ABCISemVer
 )
