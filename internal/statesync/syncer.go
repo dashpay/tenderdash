a40b882ae51601fb6e6ece6bd7bec3e5d909794b
shotonoff <dmitrii.golubev@dash.org>
2023-07-21 10:56:44 +0200

    chore: disable duplication lint

--- internal/statesync/syncer.go
+++ internal/statesync/syncer.go
@@ -387,7 +387,7 @@ func (s *syncer) Sync(ctx context.Context, snapshot *snapshot, queue *chunkQueue
 
 // offerSnapshot offers a snapshot to the app. It returns various errors depending on the app's
 // response, or nil if the snapshot was accepted.
-func (s *syncer) offerSnapshot(ctx context.Context, snapshot *snapshot) error {
+func (s *syncer) offerSnapshot(ctx context.Context, snapshot *snapshot) error { //nolint:dupl
 	s.logger.Info("Offering snapshot to ABCI app",
 		"height", snapshot.Height,
 		"version", snapshot.Version,
