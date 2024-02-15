c77d710135ff3dcde6d9ff825788deede689ff5f
Lukasz Klimek <842586+lklimek@users.noreply.github.com>
2024-02-15 16:20:18 +0100

    chore(mempool): remove unnecessary tx hash calculation

--- internal/mempool/mempool.go
+++ internal/mempool/mempool.go
@@ -486,7 +486,7 @@ func (txmp *TxMempool) addNewTransaction(wtx *WrappedTx, checkTxRes *abci.Respon
 		txmp.logger.Info(
 			"rejected bad transaction",
 			"priority", wtx.Priority(),
-			"tx", fmt.Sprintf("%X", wtx.tx.Hash()),
+			"tx", fmt.Sprintf("%X", wtx.hash),
 			"peer_id", wtx.peers,
 			"code", checkTxRes.Code,
 			"post_check_err", err,
@@ -521,13 +521,13 @@ func (txmp *TxMempool) addNewTransaction(wtx *WrappedTx, checkTxRes *abci.Respon
 			w := elt.Value.(*WrappedTx)
 			txmp.logger.Debug(
 				"rejected valid incoming transaction; tx already exists for sender",
-				"tx", fmt.Sprintf("%X", w.tx.Hash()),
+				"tx", fmt.Sprintf("%X", w.hash),
 				"sender", sender,
 			)
 			txmp.metrics.RejectedTxs.Add(1)
 			// TODO(creachadair): Report an error for a duplicate sender.
 			// This is an API change, unfortunately, but should be made safe if it isn't.
-			// fmt.Errorf("transaction rejected: tx already exists for sender %q (%X)", sender, w.tx.Hash())
+			// fmt.Errorf("transaction rejected: tx already exists for sender %q (%X)", sender, w.hash)
 			return nil
 		}
 	}
@@ -567,7 +567,7 @@ func (txmp *TxMempool) addNewTransaction(wtx *WrappedTx, checkTxRes *abci.Respon
 			})
 
 			txmp.logger.Debug("evicting lower-priority transactions",
-				"new_tx", tmstrings.LazySprintf("%X", wtx.tx.Hash()),
+				"new_tx", tmstrings.LazySprintf("%X", wtx.hash),
 				"new_priority", priority)
 
 			// Evict as many of the victims as necessary to make room.
@@ -593,13 +593,13 @@ func (txmp *TxMempool) addNewTransaction(wtx *WrappedTx, checkTxRes *abci.Respon
 			txmp.cache.Remove(wtx.tx)
 			txmp.logger.Error(
 				"rejected valid incoming transaction; mempool is full",
-				"tx", fmt.Sprintf("%X", wtx.tx.Hash()),
+				"tx", fmt.Sprintf("%X", wtx.hash),
 				"err", err.Error(),
 			)
 			txmp.metrics.RejectedTxs.Add(1)
 			// TODO(creachadair): Report an error for a full mempool.
 			// This is an API change, unfortunately, but should be made safe if it isn't.
-			// fmt.Errorf("transaction rejected: mempool is full (%X)", wtx.tx.Hash())
+			// fmt.Errorf("transaction rejected: mempool is full (%X)", wtx.hash)
 			return nil
 		}
 	}
@@ -622,7 +622,7 @@ func (txmp *TxMempool) addNewTransaction(wtx *WrappedTx, checkTxRes *abci.Respon
 	txmp.logger.Debug(
 		"inserted new valid transaction",
 		"priority", wtx.Priority(),
-		"tx", tmstrings.LazySprintf("%X", wtx.tx.Hash()),
+		"tx", tmstrings.LazySprintf("%X", wtx.hash),
 		"height", txmp.height,
 		"num_txs", txmp.Size(),
 	)
@@ -649,7 +649,7 @@ func (txmp *TxMempool) evict(size int64, victims []*clist.CElement) bool {
 
 		txmp.logger.Debug(
 			"evicted valid existing transaction; mempool full",
-			"old_tx", tmstrings.LazySprintf("%X", w.tx.Hash()),
+			"old_tx", tmstrings.LazySprintf("%X", w.hash),
 			"old_priority", w.priority,
 		)
 		txmp.removeTxByElement(vic)
@@ -711,7 +711,7 @@ func (txmp *TxMempool) handleRecheckResult(tx types.Tx, checkTxRes *abci.Respons
 	txmp.logger.Debug(
 		"existing transaction no longer valid; failed re-CheckTx callback",
 		"priority", wtx.Priority(),
-		"tx", fmt.Sprintf("%X", wtx.tx.Hash()),
+		"tx", fmt.Sprintf("%X", wtx.hash),
 		"err", err,
 		"code", checkTxRes.Code,
 	)
@@ -759,7 +759,7 @@ func (txmp *TxMempool) recheckTransactions(ctx context.Context) {
 				})
 				if err != nil {
 					txmp.logger.Error("failed to execute CheckTx during recheck",
-						"err", err, "hash", fmt.Sprintf("%x", wtx.tx.Hash()))
+						"err", err, "hash", fmt.Sprintf("%x", wtx.hash))
 				} else {
 					txmp.handleRecheckResult(wtx.tx, rsp)
 				}
