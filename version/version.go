2d4a736885ed4b515f810b421cb7f41a6091db5b
Lukasz Klimek <842586+lklimek@users.noreply.github.com>
2024-07-25 12:41:59 +0200

    feat(abci): allow overriding genesis time in InitChain

--- version/version.go
+++ version/version.go
@@ -11,7 +11,7 @@ const (
 	// when not using git describe. It is formatted with semantic versioning.
 	TMVersionDefault = "1.1.0-dev.2"
 	// ABCISemVer is the semantic version of the ABCI library
-	ABCISemVer = "1.0.0"
+	ABCISemVer = "1.1.0"
 
 	ABCIVersion = ABCISemVer
 )
