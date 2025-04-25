334ee33a11df60f95aa5630a51ed44dc48454b3d
lklimek <842586+lklimek@users.noreply.github.com>
2025-04-25 16:05:11 +0200

    build(deps): major update of mockery to 3.2.4 (#1107)
    * build(deps): upgrade mockery to 3.2.4
    
    * fix: non-existing elements in mockery config
    
    * chore: make mockery
    
    * chore: make mockery

--- scripts/mockery_generate.sh
+++ scripts/mockery_generate.sh
@@ -6,9 +6,9 @@
 # runs the published Docker container. This legerdemain is so that the CI build
 # and a local build can work off the same script.
 #
-VERSION=v2.53.3
+VERSION=v3.2.4
 
-if ! mockery --version 2>/dev/null | grep $VERSION; then
+if [ "$(mockery version)" != "$VERSION" ]; then
   echo "Please install mockery $VERSION, example for Linux x86_64:"
   echo "wget https://github.com/vektra/mockery/releases/download/${VERSION}/mockery_${VERSION#v}_Linux_x86_64.tar.gz"
   exit 1
