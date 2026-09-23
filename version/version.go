49a0de1e9180710874cee4618496fedae10a15fc
Lukasz Klimek <842586+lklimek@users.noreply.github.com>
2026-09-23 11:07:14 +0000

    chore(release): update changelog and version to 1.8.1

--- version/version.go
+++ version/version.go
@@ -9,7 +9,7 @@ var (
 const (
 	// TMVersionDefault is the used as the fallback version for Tenderdash
 	// when not using git describe. It is formatted with semantic versioning.
-	TMVersionDefault = "1.8.0"
+	TMVersionDefault = "1.8.1"
 	// ABCISemVer is the semantic version of the ABCI library
 	ABCISemVer = "1.4.0"
 
